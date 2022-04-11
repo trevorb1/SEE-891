eefd57758ea24fa4b973e9b1bec0061eb238eb24
trevorb1 <trevor_barnes@sfu.ca>
2022-04-11 08:44:55 -0700

    added tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,11 +1,10 @@
 '''Module holding tests for example package.'''
 
-from example_package.example import add_one
-from example_package.example import add_five
+from example_package import example
 
 
 def test_add_one():
-    assert add_one(8) == 9
+    assert example.add_one(8) == 9
 
 def test_add_five():
-    assert add_five(-10) == -5
+    assert example.add_five(-10) == -5
